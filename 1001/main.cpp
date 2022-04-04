07de36d472c077111a4b7b67c2e1a4d60d3acf98
Gabriel Sá <gabriel10043@live.com>
2022-04-04 16:18:49 -0300

    Create main.cpp

---
+++ 1001/main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+ 
+using namespace std;
+ 
+int main() {
+ 
+    int A, B;
+    
+    scanf("%d", &A);
+    scanf("%d", &B);
+    
+    printf("X = %d\n", A+B);
+ 
+    return 0;
+}
